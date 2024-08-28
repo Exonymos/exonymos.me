01aed8239741e8dc75336b2e3fb8110f7892c9a3
Joy Barot <joybarot720@gmail.com>
2024-08-28 09:35:46 +0530

    Update layout.tsx

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,21 +1,9 @@
-"use client";
-
-import { useEffect } from 'react';
-
 export default function ProjectsLayout({
 	children,
 }: { children: React.ReactNode }) {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://cdn.commento.io/js/commento.js';
-        script.defer = true;
-        document.body.appendChild(script);
-    }, []);
-
 	return (
-		<div className="relative min-h-screen bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900">
+		<div className="relative min-h-screen bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900 ">
 			{children}
-            <div id="commento" className="p-4 md:p-8"></div>
 		</div>
 	);
 }
