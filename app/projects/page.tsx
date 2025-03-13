5f6aa37fe5a1d5eddf85c7c123fa57210be0f602
Joy Barot <joybarot720@gmail.com>
2025-03-13 22:58:42 +0530

    update featured project to snake-game-adventure

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -21,7 +21,7 @@ export default async function ProjectsPage() {
   }, {} as Record<string, number>);
 
   const featured = allProjects.find((project) => project.slug === "tea-addicts")!;
-  const top2 = allProjects.find((project) => project.slug === "gacha-level-calculator")!;
+  const top2 = allProjects.find((project) => project.slug === "snake-game-adventure")!;
   const top3 = allProjects.find((project) => project.slug === "exonymos.me")!;
   const sorted = allProjects
     .filter((p) => p.published)
